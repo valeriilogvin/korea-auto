73a6ef23b0a103552dd8b86676d48bf389302179
Valerii Logvin <valerii.logvin@gmail.com>
2021-02-02 11:57:01 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
 
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+
     let $pane_block__header = $('.pane_block__header'),
         pane_block__sub = '.pane_block__sub',
         $pane_block__sub = $('.pane_block__sub'),
@@ -43,7 +66,11 @@ $(document).ready(function () {
             dots: true,
             speed: 300,
             slidesToShow: 1,
+            autoplay: true,
+            pauseOnHover: false,
+            pauseOnDotsHover: true,
             adaptiveHeight: true,
+            autoplaySpeed: 4000,
             dotsClass: 'my-slick-dots-v',
             prevArrow: prevArrow_v,
             nextArrow: nextArrow_v,
@@ -74,6 +101,7 @@ $(document).ready(function () {
         .slick({
             infinite: true,
             autoplay: true,
+            pauseOnHover: false,
             autoplaySpeed: 3000,
             dots: false,
             arrows: false,
@@ -250,4 +278,70 @@ $(document).ready(function () {
         $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 500);
         e.preventDefault();
     });
+
+    //Фиксированное меню
+    var navbar = document.getElementById('navbarId');
+
+    var offsetNavbar = navbar.offsetTop;
+
+    window.onscroll = function(e) {
+        if(window.scrollY > offsetNavbar) {
+            if(!navbar.classList.contains('position-fixed')) {
+                navbar.classList.add('position-fixed');
+                navbar.classList.add('bs-navbar');
+            }
+        } else {
+            if(navbar.classList.contains('position-fixed')) {
+                navbar.classList.remove('position-fixed');
+                navbar.classList.remove('bs-navbar');
+            }
+        }
+    };
+
+    // Animations
+
+    const animItems = document.querySelectorAll('._anim-items');
+
+    if (animItems.length > 0) {
+
+        window.addEventListener('scroll', animOnScroll);
+
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 3;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-off')) {
+                        animItem.classList.remove('_active');
+                    }
+                }
+            }
+        }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {
+                top: rect.top + scrollTop,
+                left: rect.left + scrollLeft
+            }
+        }
+
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
+
+    }
 });
